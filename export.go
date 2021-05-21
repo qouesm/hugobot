328860718938c8d2ecf4ba760ad74f55e1b6b29a
qouesm <58123522+qouesm@users.noreply.github.com>
2021-05-21 07:51:17 +0000

    Update export.go

--- export.go
+++ export.go
@@ -4,9 +4,8 @@ import "gitlab.com/qouesm/hugobot/commands"
 
 func exportCommands() []commands.Command {
 	return []commands.Command{
-		// commands.ClassClear,
-		// commands.Embed,
-		commands.Ping,
+		commands.ClassClear,
+		// commands.Ping,
 		// commands.Quietping,
 		commands.ReactRoles,
 		// commands.Uhoh,
